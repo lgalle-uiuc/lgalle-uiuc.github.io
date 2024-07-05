85f7caf93ab696268ad8b908ce605624f86c71ba
Luke Galle <lukegalle@lukes-mbp.lan>
2024-07-05 19:52:29 -0400

    page data

--- scripts/page_data.js
+++ scripts/page_data.js
@@ -15,7 +15,7 @@ async function load(num_cylinders, screen) {
     console.log(num_cylinders)
     console.log(screen)
 
-    d3.select("#page_chart").html("");
+    d3.select("#page_data").html("");
 
     let raw_data = await d3.csv("../resources/cars2017.csv");
 
@@ -69,7 +69,7 @@ async function load(num_cylinders, screen) {
     })]);
 
     // append the svg object to the body of the page
-    var svg = d3.select("#page_chart").append("svg")
+    var svg = d3.select("#page_data").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -103,7 +103,8 @@ async function load(num_cylinders, screen) {
             div.transition()
                 .duration(100)
                 .style("opacity", 1);
-            div.html(d.Make)
+            div.html('<div><p>Make: ' + d.Make + '</p><p>AverageHighwayMPG: ' + d.AverageHighwayMPG
+                    + '</p><p>AverageCityMPG: ' + d.AverageCityMPG + '</p><p>Cylinders: ' + d.EngineCylinders + '</p></div>')
                 .style("left", (d3.event.pageX + 10) + "px")
                 .style("top", (d3.event.pageY - 15) + "px");
         })
